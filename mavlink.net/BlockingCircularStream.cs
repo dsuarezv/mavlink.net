cb4c4cd6594727e31f5b3fbd74ce56172030a6e9
David Suarez <dsuarezv@gmail.com>
2013-11-19 23:18:20 +0100

    Finished implementation of BlockingCircularStream

--- mavlink.net/BlockingCircularStream.cs
+++ mavlink.net/BlockingCircularStream.cs
@@ -40,20 +40,22 @@ namespace System.IO
         private int mReadPosition;
         private int mCapacity;
         private AutoResetEvent mBlockSignal = new AutoResetEvent(false);
+        private bool mIsAborted = false;
 
 
         public BlockingCircularStream(int bufferCapacity)
         {
             mCapacity = bufferCapacity;
             mBuffer = new byte[bufferCapacity];
-            BlockingTimeoutMs = 1000;
-            
-            Reset();
         }
 
-        public int BlockingTimeoutMs
+        protected override void Dispose(bool disposing)
         {
-            get; set;
+            base.Dispose(disposing);
+
+            AbortRead();
+
+            mBlockSignal.Dispose();
         }
 
         public override bool CanRead
@@ -80,13 +82,16 @@ namespace System.IO
         {
             get
             {
-                if (mWritePosition > mReadPosition)
+                lock (this)
                 {
-                    return mWritePosition - mReadPosition;
-                }
-                else
-                {
-                    return mCapacity - mReadPosition + mWritePosition;
+                    if (mWritePosition >= mReadPosition)
+                    {
+                        return mWritePosition - mReadPosition;
+                    }
+                    else
+                    {
+                        return mCapacity - mReadPosition + mWritePosition;
+                    }
                 }
             }
         }
@@ -103,44 +108,81 @@ namespace System.IO
             }
         }
 
+        /// <summary>
+        /// Returns 'count' bytes from the stream into 'buffer'.
+        /// </summary>
+        /// <param name="buffer">The buffer that will receive the read bytes</param>
+        /// <param name="offset">offset in the result buffer to start copying read bytes</param>
+        /// <param name="count">Number of bytes to be copied from the stream into buffer</param>
+        /// <returns>The number of bytes read. or -1 if the Read operation is 
+        /// aborted (by calling AbortRead() from another thread)</returns>
+        /// <remarks>
+        /// The read operation will return 'count' bytes from the stream if they
+        /// are available. Otherwise, this method will block until enough bytes
+        /// are written through the Write() method from another thread.
+        /// </remarks>
         public override int Read(byte[] buffer, int offset, int count)
         {
             if (count > mCapacity)
                 throw new IndexOutOfRangeException("Tried to read more bytes than the capacity of the circular buffer.");
 
+            mIsAborted = false;
+
             // wait until the buffer has enough bytes available
             while (Length < count)
             {
-                mBlockSignal.WaitOne(BlockingTimeoutMs);
+                mBlockSignal.WaitOne();
+                if (mIsAborted) return -1;
             }
 
-            if (mReadPosition + count <= mCapacity)
+            lock (this)
             {
-                // Direct copy
-                CopyBytes(mBuffer, buffer, mReadPosition, offset, count);
-                mReadPosition += count;
+                if (mReadPosition + count <= mCapacity)
+                {
+                    // Direct copy
+                    CopyBytes(mBuffer, buffer, mReadPosition, offset, count);
+                    mReadPosition += count;
+                }
+                else
+                {
+                    // Split buffer (new round)
+                    int len1 = mCapacity - mReadPosition;
+                    int len2 = count - len1;
+
+                    CopyBytes(mBuffer, buffer, mReadPosition, offset, len1);
+                    CopyBytes(mBuffer, buffer, 0, offset + len1, len2);
+
+                    mReadPosition = len2;
+                }
+
+                return count;
             }
-            else
-            {
-                // Split buffer (new round)
-                int len1 = mCapacity - mReadPosition;
-                int len2 = count - len1;
+        }
 
-                CopyBytes(mBuffer, buffer, mReadPosition, offset, len1);
-                CopyBytes(mBuffer, buffer, 0, offset + len1, len2);
-
-                mReadPosition = len2;
-            }
-
-            return count;
+        /// <summary>
+        /// Aborts a pending Read() operation if one is in progress. 
+        /// </summary>
+        /// <remarks>If no Read() method is waiting for bytes to arrive in the stream, 
+        /// this method does nothing. In any case, this method returns inmediately.</remarks>
+        public void AbortRead()
+        {
+            mIsAborted = true;
+            mBlockSignal.Set();
         }
 
         public override int ReadByte()
         {
-            if (mReadPosition == mWritePosition) return -1;
-            if (mReadPosition == mCapacity) mReadPosition = 0;
+            while (mReadPosition == mWritePosition)
+            {
+                mBlockSignal.WaitOne();
+            }
 
-            return mBuffer[mReadPosition++];
+            lock (this)
+            {
+                if (mReadPosition == mCapacity) mReadPosition = 0;
+
+                return mBuffer[mReadPosition++];
+            }
         }
 
         public override long Seek(long offset, SeekOrigin origin)
@@ -158,39 +200,45 @@ namespace System.IO
             if (count > mCapacity)
                 throw new IndexOutOfRangeException("Tried to write more bytes than the capacity of the circular buffer.");
 
-            if (mWritePosition + count <= mCapacity)
+            lock (this)
             {
-                // Direct copy
-                CopyBytes(buffer, mBuffer, offset, mWritePosition, count);
-                mWritePosition += count;
-            }
-            else
-            {
-                // Split buffer (new round)
-                int len1 = mCapacity - mWritePosition;
-                int len2 = count - len1;
+                if (mWritePosition + count <= mCapacity)
+                {
+                    // Direct copy
+                    CopyBytes(buffer, mBuffer, offset, mWritePosition, count);
+                    mWritePosition += count;
+                }
+                else
+                {
+                    // Split buffer (new round)
+                    int len1 = mCapacity - mWritePosition;
+                    int len2 = count - len1;
 
-                if (len2 > mReadPosition)
-                    throw new Exception("Data is being overwritten without being read. May need to increase capacity.");
+                    if (len2 > mReadPosition)
+                        throw new Exception("Data is being overwritten without being read. May need to increase capacity.");
 
-                CopyBytes(buffer, mBuffer, offset, mWritePosition, len1);
-                CopyBytes(buffer, mBuffer, offset + len1, 0, len2);
+                    CopyBytes(buffer, mBuffer, offset, mWritePosition, len1);
+                    CopyBytes(buffer, mBuffer, offset + len1, 0, len2);
 
-                mWritePosition = len2;
+                    mWritePosition = len2;
+                }
             }
 
             mBlockSignal.Set();
         }
 
+        public override string ToString()
+        {
+            return string.Format("{0}: r:{1} w:{2} l:{3}",
+                System.Text.Encoding.UTF8.GetString(mBuffer),
+                mReadPosition,
+                mWritePosition,
+                Length);
+        }
+
 
         // __ Impl ____________________________________________________________
 
-
-        private void Reset()
-        {
-            mWritePosition = 0;
-            mReadPosition = 0;
-        }
 
         private void CopyBytes(byte[] source, byte[] destination, int sourceStart, int destinationStart, int count)
         {
